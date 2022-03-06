a1c37b3ed9c28581f05d626ea1b3f5a1313ceb42
Phil Patterson <57622286+Crow2525-pp@users.noreply.github.com>
2022-03-06 12:42:05 +1100

    Version 1.0
    File works as intended.   
    Future ideas to do: allow user to select their Destination and Target folder locations.

---
+++ CreateDOLFolder.ps1
@@ -0,0 +1,23 @@
+$dest = "G:\QBE Shared Information\Prof & Financial Lines\Referrals\DOL"
+$template = "G:\QBE Shared Information\Prof & Financial Lines\Referrals\DOL\__TEMPLATE"
+$Desc = Read-Host "Enter Insured Name, e.g. QBE Australia Pty Ltd"
+$Dest2 = "Analysis & QBE Terms" 
+$Dir = Read-Host "Enter Policy Year, e.g 2021-2022 or 2021-22 (up to you)"
+$Attach = Read-Host "Enter Attachment e.g 5xs5" 
+$NewDir = $Desc.ToUpper()
+$NewDest = $dest, $NewDir -join "\"
+
+New-Item -Path $dest -ItemType "Directory" -Name $newDir
+GCI -Path $template -Recurse |
+   Copy-Item -Destination {
+      if ($_.PSIsContainer) {
+         Join-Path $newdest $_.Parent.FullName.Substring($template.length)
+         } else {
+               Join-Path $newdest $_.FullName.Substring($template.length)
+        }
+    }
+
+
+$TermDir =Join-Path $dest -ChildPath $Desc | Join-Path -ChildPath $Dest2
+$items=Get-ChildItem $TermDir
+$items | Rename-Item -NewName { $_.Basename + "_" + $Dir +"_" + $NewDir +"_" + $Attach + $_.extension}
